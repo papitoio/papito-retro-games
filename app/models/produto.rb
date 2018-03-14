83d67be5d2bd1a5c3745b62f7f0cdd772fa441c9
Fernando Papito <fernando@qaninja.io>
2018-03-13 22:04:58 -0300

    corrindo o bug, alterei o limite de caracteres na model produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,5 @@
 class Produto < ApplicationRecord
-    validates :nome, length: { minimum: 10 }, uniqueness: true
+    validates :nome, length: { minimum: 15 }, uniqueness: true
     validates :descricao, length: { minimum: 10, maximum: 250 }
     validates :quantidade, presence: true
     validates :preco, presence: true
